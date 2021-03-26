bee43d15bf83369a79b1ed876598338eef4545d3
Rob J Hyndman <robjhyndman@gmail.com>
2021-03-26 11:53:20 +1100

    Minimal example working

--- minimal_example.R
+++ minimal_example.R
@@ -1,45 +1,50 @@
 library(tidyverse)
 library(tsibble)
 library(fable)
-library(fabletools)
 library(feasts)
 library(lubridate)
 library(fable.tscount)
-#library(tscount)
+
+f_horizon <- 5 * 7
 
 # Read data
 incidents_original <- readxl::read_excel("data/Nature_of_Incidents_Attended.xlsx") %>%
   mutate(date = as_date(Incident_Date)) %>%
   janitor::clean_names() %>%
-  force_tz(date, tz = "GB") 
+  force_tz(date, tz = "GB")
 # prepare data
-nature_of_incident_low <- c("AUTOMATIC CRASH NOTIFICATION",
-                            "INACCESSIBLE INCIDENT/OTHER ENTRAP",
-                            "INTERFACILITY EVALUATION/TRANSFER",
-                            "MAJOR INCIDENT - OVERRIDE PROQA",
-                            "TRANSFER/INTERFACILITY/PALLIATIVE")
-incidents <- incidents_original %>%   mutate_at(.vars = "nature_of_incident", .funs=as.numeric) %>% 
-  mutate(nature_of_incident= ifelse(!is.na(nature_of_incident),nature_of_incident_description , "upgrade")) %>% 
-  mutate(nature_of_incident= ifelse(nature_of_incident %in% nature_of_incident_low,"other" , nature_of_incident)) %>% 
-  group_by(lhb_code, category, nature_of_incident, date) %>% 
-  summarise(incidents = sum(total_incidents),.groups = "drop") 
-
-# create tsibble
-incidents_tsbl <- incidents %>%  
+nature_of_incident_low <- c(
+  "AUTOMATIC CRASH NOTIFICATION",
+  "INACCESSIBLE INCIDENT/OTHER ENTRAP",
+  "INTERFACILITY EVALUATION/TRANSFER",
+  "MAJOR INCIDENT - OVERRIDE PROQA",
+  "TRANSFER/INTERFACILITY/PALLIATIVE"
+)
+incidents <- incidents_original %>%
+  mutate(
+    nature_of_incident = as.numeric(nature_of_incident),
+    nature_of_incident = ifelse(!is.na(nature_of_incident), nature_of_incident_description, "upgrade"),
+    nature_of_incident = ifelse(nature_of_incident %in% nature_of_incident_low, "other", nature_of_incident)
+  ) %>%
+  group_by(lhb_code, category, nature_of_incident, date) %>%
+  summarise(incidents = sum(total_incidents), .groups = "drop") %>%
   as_tsibble(index = date, key = c(lhb_code, category, nature_of_incident)) %>%
-  fill_gaps(incidents = 0, .full = TRUE) %>% 
-  mutate(category = factor(category, level=c("RED","AMBER","GREEN")),
-         nature_of_incident = factor(nature_of_incident),
-         lhb_code = factor(lhb_code))
+  fill_gaps(incidents = 0, .full = TRUE) %>%
+  mutate(
+    category = factor(category, level = c("RED", "AMBER", "GREEN")),
+    nature_of_incident = factor(nature_of_incident),
+    lhb_code = factor(lhb_code)
+  ) %>%
+  filter(
+    (category == "RED" | category == "GREEN") &
+      (lhb_code == "AB" | lhb_code == "SB") &
+      (nature_of_incident == "BREATHING PROBLEMS" | nature_of_incident == "CHEST PAIN"),
+    date < max(date) - f_horizon
+  )
 
 # create grouped structure
-incidents_gts <- incidents_tsbl %>%
+train <- incidents %>%
   aggregate_key(nature_of_incident * category * lhb_code, incidents = sum(incidents))
-#minimal example
-incidents_gts_sample <- incidents_gts %>% 
-  filter((category == "RED" | category == "GREEN") & (lhb_code == "AB" | lhb_code == "SB") & (nature_of_incident == "BREATHING PROBLEMS" | nature_of_incident == "CHEST PAIN"))
-f_horizon <- 5*7
-train <- incidents_gts_sample %>% filter(date < max(date) - f_horizon)
 
 fit_incident <- train %>%
   model(
