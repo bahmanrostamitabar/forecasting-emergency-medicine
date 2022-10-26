b77144bc57692e443a3cb4c88b852b87e3dc6099
Rob Hyndman <robjhyndman@gmail.com>
2022-10-26 13:06:57 +1100

    Working on hts code

--- rscript/hts/forecast_hts_with_transformation.R
+++ rscript/hts/forecast_hts_with_transformation.R
@@ -1,12 +1,25 @@
 library(hts)
-library(readr)
-source("htsplus.R")
+source(here::here("rscript/hts/htsplus.R"))
 
-# Test on incident data
-incident_all <- read_rds("data/incident_all.rds")
-incident_ts <- ts(incident_all, frequency = 7)
+# Read hierarchical/grouped time series
+incident_gts <- read_rds(here::here("data/incidents_test_gts.rds"))
+#incident_gts <- read_rds(here::here("data/incidents_gts.rds"))
 
-# create hierarchical/grouped time series
-gts_incident <- hts::gts(incident_ts, characters = list(c(1, 2), 3, 9))
-fcst_bu <- htsplus(gts_incident, h = 10, model_function = ets, lambda = 0.5, method = "bu")
-fcst_wls <- htsplus(gts_incident, h = 10, model_function = ets, lambda = 0.5, method = "wls")
+train <- window(incident_gts, end = c(12,1))
+test <- window(incident_gts, start=c(12,2))
+
+# Simulated future sample paths
+fcst_ets_bu <- htsplus(train, h = nrow(test$bts), model_function = ets, lambda = 0.5, method = "bu")
+fcst_ets_wls <- htsplus(train, h = nrow(test$bts), model_function = ets, lambda = 0.5, method = "wls")
+fcst_ets_mint <- htsplus(train, h = nrow(test$bts), model_function = ets, lambda = 0.5, method = "mint")
+
+# Means
+mean_ets_bu <- t(apply(fcst_ets_bu, c(1,2), mean))
+mean_ets_wls <- t(apply(fcst_ets_wls, c(1,2), mean))
+mean_ets_mint <- t(apply(fcst_ets_mint, c(1,2), mean))
+
+rmsse(mean_ets_bu, test, train)
+rmsse(mean_ets_wls, test, train)
+rmsse(mean_ets_mint, test, train)
+
+
--- rscript/hts/htsplus.R
+++ rscript/hts/htsplus.R
@@ -10,77 +10,87 @@
 htsplus <- function(object, h,
                     model_function = auto.arima,
                     lambda = NULL, level = 95,
-                    method = c("wls", "bu"), nsim = 2000) {
+                    method = c("wls", "bu", "mint"), nsim = 2000) {
   method <- match.arg(method)
   # Generate all time series at all levels
   ally <- aggts(object)
+  nbottom <- NCOL(object$bts)
   nseries <- NCOL(ally)
+  ntime <- NROW(ally)
   # Do Box-Cox transformation if required
   if (!is.null(lambda)) {
     ally <- BoxCox(ally, lambda)
   }
-  # Set up matrices to store results
-  fmean <- fvar <- flower <- fupper <- matrix(NA, nrow = h, ncol = nseries)
+  # Set up matrices to store residuals
+  innov_res <- response_res <- matrix(0, nrow=nrow(ally), ncol=nseries)
   models <- list()
   # Loop over all time series and compute forecast means and variances
-  for (i in seq(NCOL(ally))) {
-    models[[i]] <- model_function(ally[, i])
-    fc <- forecast(models[[i]], h = h, level = level)
-    fmean[, i] <- fc$mean
-    fvar[, i] <- ((fc$upper - fc$lower) / (2 * qnorm(0.5 + level / 200)))^2
-  }
-  # Reverse Box-Cox transformation if required
-  # using Taylor series approximation (delta method) to get mean and variance
-  # on original scale
-  if (!is.null(lambda)) {
-    fmean <- InvBoxCox(fmean, lambda)
-    tmp <- lambda * fmean + 1
-    fp2 <- fmean^(2 - 2 * lambda)
-    fmean <- fmean * (1 + 0.5 * fvar * (1 - lambda) / (fmean)^(2 * lambda))
-    fvar <- fvar * fp2
+  for (i in seq(nseries)) {
+    # Add some tiny positive noise to avoid zero series
+    models[[i]] <- model_function(ally[, i] + abs(rnorm(n=ntime, sd=1e-2)))
+    # Grab the residuals
+    innov_res[,i] <- residuals(models[[i]], type="innovation")
+    response_res[,i] <- residuals(models[[i]], type="response")
   }
   # S matrix
   S <- smatrix(object)
+  # Find W matrix
+  if (method == "ols") {
+    W <- diag(nseries)
+  } else {
+    W <- hts:::lowerD(response_res)
+    if (method == "mint") {
+      W <- hts:::shrink.estim(response_res, W)[[1]]
+    }
+  }
   # Find G matrix
-  if (method == "wls") {
-    Lambda <- diag(1 / fvar[1, ])
-    G <- solve(t(S) %*% Lambda %*% S) %*% t(S) %*% Lambda
-  } else {
-    # Bottom up
-    nbottom <- NCOL(object$bts)
+  if (method == "bu") {
     G <- cbind(matrix(0, nrow = nbottom, ncol = nseries - nbottom), diag(nrow = nbottom))
-  }
-  # Now compute ytilde and its variance
-  SG <- S %*% G
-  ytilde <- fmean %*% t(SG)
-  vartilde <- ytilde * NA
-  for (i in seq(h)) {
-    vartilde[i, ] <- diag(SG %*% diag(fvar[i, ]) %*% t(SG))
-  }
-  # Compute prediction intervals using nonparametric method if lambda not null
-  if (is.null(lambda)) {
-    # Use normally distributed results
-    # note for myself:
-    # 1.what is 200?, can we change 0.5 to get different quantiles?
-    # 2. If we use other distributions, like poisson
-    # 2.1. lower <- qpois(0.025, lambda = ytilde)
-    # 2.2. upper <- qpois(0.975, lambda = ytilde)
-    flower <- qnorm(0.5 - level / 200, mean = ytilde, sd = sqrt(vartilde))
-    fupper <- qnorm(0.5 + level / 200, mean = ytilde, sd = sqrt(vartilde))
   } else {
-    sim <- array(0, c(nseries, h, nsim))
-    for (i in seq(nseries)) {
-      for (j in seq(nsim)) {
-        sim[i, , j] <- simulate(models[[i]], nsim = h)
-      }
-    }
-    sim <- InvBoxCox(sim, lambda)
-    for (j in seq(nsim)) {
-      sim[, , j] <- SG %*% sim[, , j]
-    }
-    flower <- t(apply(sim, c(1, 2), quantile, prob = 0.5 - level / 200))
-    fupper <- t(apply(sim, c(1, 2), quantile, prob = 0.5 + level / 200))
+    Winv <- solve(W)
+    G <- solve(t(S) %*% Winv %*% S) %*% t(S) %*% Winv
   }
+  SG <- S %*% G
+
+  # Compute prediction intervals using nonparametric method
+  sim <- array(0, c(nseries, h, nsim))
+  for (j in seq(nsim)) {
+    bootres <- innov_res[sample(ntime, size=h, replace=TRUE),]
+    for (i in seq(nseries)) {
+      sim[i, , j] <- simulate(models[[i]], innov = bootres[,i])
+    }
+  }
+  # Backtransform
+  if (!is.null(lambda)) {
+    sim <- InvBoxCox(sim, lambda)
+  }
+  # Project simulated sample paths to coherent space
+  for (j in seq(nsim)) {
+    sim[, , j] <- SG %*% sim[, , j]
+  }
+  # Compute reconciled means, variances and quantiles
+  #fmean <- t(apply(sim, c(1,2), mean))
+  #fvar <- t(apply(sim, c(1,2), var))
+  #flower <- t(apply(sim, c(1, 2), quantile, prob = 0.5 - level / 200))
+  #fupper <- t(apply(sim, c(1, 2), quantile, prob = 0.5 + level / 200))
+
   # Return mean, variance and lower and upper limits of prediction interval
-  list(mean = ytilde, var = vartilde, lower = flower, upper = fupper)
+  #list(mean = ytilde, var = fvar, lower = flower, upper = fupper)
+  
+  # Return simulated reconciled sample paths
+  dimnames(sim)[[1]] <- colnames(ally)
+  dimnames(sim)[[2]] <- paste0("h=",seq(h))
+  dimnames(sim)[[3]] <- paste0("sim",seq(nsim))
+  return(sim)
+}
+
+# Look at RMSSE over several levels for MinT
+rmsse <- function(fmean, test_gts, train_gts) {
+  alltest <- aggts(test_gts)
+  alltrain <- aggts(train_gts)
+  scale_factor <- colMeans(diff(alltrain, 7)^2)
+  rmsse <- colMeans(sweep((fmean - alltest)^2, 2L, scale_factor, '/'))
+  rmsse[scale_factor < .Machine$double.eps] <- 0
+  return(rmsse)
+}
 }
