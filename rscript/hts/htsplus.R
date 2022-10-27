c5354b6fdbb3c7a365178036452e0874a22e5e2f
Rob J Hyndman <robjhyndman@gmail.com>
2022-10-27 16:37:28 +1100

    Refactored simulation code to store everything on disk

--- rscript/hts/htsplus.R
+++ rscript/hts/htsplus.R
@@ -1,44 +1,104 @@
-# htsplus = Use forecast reconciliation to generate simulated future sample paths
+# fit_models: Function to fit models to incidents data and save results as a list
 
 # object = gts object
-# h = forecast horizon
-# model_function = function used to model each time series. e.g., ets or auto.arima or tscount
-# methods = vector of reconcilation methods. All will be used
-# nsim = number of simulations in computing prediction intervals using nonparametric method
+# model_function = function used to model each time series.
+# e.g., ets or auto.arima or tscount
 
-htsplus <- function(object,
-                    h,
-                    model_function = auto.arima,
-                    methods,
-                    nsim = 2000) {
+fit_models <- function(object, model_function = "ets") {
+  # Number of observations in training set
+  ntime <- NROW(object$bts)
+
+  # Form file name for saving results
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, ".rds"))
+  # Check if this has already been run
+  if (fs::file_exists(filename)) {
+    # Just use the previous results
+    return(invisible(read_rds(filename)))
+  }
+
   # Generate all time series at all levels
   ally <- aggts(object)
   # Loop over all time series and store models
   fit_model <- function(y, model_function) {
     ntime <- length(y)
+    modelfn <- get(model_function)
     if (forecast:::is.constant(y)) {
       # Series is constant. So just fit a mean zero model with small variance
       return(Arima(y + rnorm(ntime, sd = 1e-3), order = c(0, 0, 0), include.mean = FALSE))
     } else {
-      return(model_function(y))
+      return(modelfn(y))
     }
   }
   models <- furrr::future_map(as.list(ally), fit_model,
     model_function = model_function,
     .options = furrr_options(seed = NULL)
   )
-  # Grab the residuals from each model
-  nseries <- NCOL(ally)
-  ntime <- NROW(ally)
-  innov_res <- response_res <- matrix(0, nrow = ntime, ncol = nseries)
-  for (i in seq(nseries)) {
-    response_res[, i] <- residuals(models[[i]], type = "response")
-    if (!identical(model_function, tscount)) {
-      innov_res[, i] <- residuals(models[[i]], type = "innovation")
-    }
+  # Save results to file and then return them
+  write_rds(models, filename)
+  return(invisible(models))
+}
+
+# make_mapping_matrices: Function to create mapping matrices for incident data
+# and save results as a list
+
+# object = gts object
+# model_function = output from fit_models()
+# type = Type of residual to store
+
+calculate_residuals <- function(object, model_function, type=c("innovation","response")) {
+  # Type of residual to store
+  type <- match.arg(type)
+  
+  ntime <- NROW(object$bts)
+  
+  # Form file name for saving results
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, "_res_",type,".rds"))
+  # Check if this has already been run
+  if (fs::file_exists(filename)) {
+    return(read_rds(filename))
   }
-  # S matrix
+  
+  # Grab models
+  models <- fit_models(object, model_function)
+  ntime <- NROW(object$bts)
+  nseries <- length(models)
+  
+  # Compute the residuals from each model
+  res <- matrix(0, nrow = ntime, ncol = nseries)
+  for (i in seq(nseries)) {
+    res[, i] <- residuals(models[[i]], type = type)
+  }
+  
+  # Save results to file and then return them
+  write_rds(res, filename)
+  return(invisible(res))
+}
+
+
+# make_mapping_matrices: Function to create mapping matrices for incident data
+# and save results as a list
+
+# object = gts object
+# model_function = function used to model each time series.
+
+make_mapping_matrices <- function(object, model_function) {
+  ntime <- NROW(object$bts)
+  
+  # Form file name for saving results
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, "_mapping.rds"))
+  # Check if this has already been run
+  if (fs::file_exists(filename)) {
+    # Just use the previous results
+    return(invisible(read_rds(filename)))
+  }
+
+  # Grab the residuals from each model
+  response_res <- calculate_residuals(object, model_function, type = "response")
+  nseries <- NCOL(response_res)
+  
+  # Create S matrix
   S <- smatrix(object)
+
   # Find W matrices
   W1 <- diag(nseries)
   W2 <- hts:::lowerD(response_res)
@@ -57,37 +117,109 @@ htsplus <- function(object,
   M2 <- S %*% G2
   M3 <- S %*% G3
 
+  # Return list of matrices
+  mapping_matrices <- list(M1 = M1, M2 = M2, M3 = M3)
+
+  # Save results to file and then return them
+  write_rds(mapping_matrices, filename)
+  return(invisible(mapping_matrices))
+}
+
+# future_sample_paths = Generate simulated future sample paths for each model
+
+# object = gts object
+# h = forecast horizon
+# model_function = function used to model each time series. e.g., ets or auto.arima or tscount
+# nsim = number of simulations in computing prediction intervals using nonparametric method
+
+future_sample_paths <- function(object, model_function = "ets", h = 84, nsim = 1000) {
+  ntime <- NROW(object$bts)
+
+  # Form file name for saving results
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, "_sim.rds"))
+  # Check if this has already been run
+  if (fs::file_exists(filename)) {
+    # Just use the previous results
+    return(invisible(read_rds(filename)))
+  }
+  
+  models <- fit_models(object, model_function)
+  restype <- dplyr::if_else(identical(model_function,"tscount"), "response", "innovation")
+  innov_res <- calculate_residuals(object, model_function, type = restype)
+  nseries <- length(models)
+
   # Compute reconciled future sample paths using cross-sectional bootstrap
-  nmethods <- length(methods)
-  sim <- array(0, c(nseries, h, nsim, nmethods))
-  dimnames(sim)[[1]] <- colnames(ally)
+  sim <- array(0, c(nseries, h, nsim))
+  dimnames(sim)[[1]] <- names(models)
   dimnames(sim)[[2]] <- paste0("h=", seq(h))
   dimnames(sim)[[3]] <- paste0("sim", seq(nsim))
-  dimnames(sim)[[4]] <- methods
   for (j in seq(nsim)) {
     bootres <- innov_res[sample(ntime, size = h, replace = TRUE), ]
     for (i in seq(nseries)) {
-      sim[i, , j, 1] <- simulate(models[[i]], innov = bootres[, i])
-      # Use same sample paths for other reconciliation methods
-      for (k in seq(nmethods)[-1]) {
-        sim[i, , j, k] <- sim[i, , j, 1]
-      }
+      sim[i, , j] <- simulate(models[[i]], innov = bootres[, i])
     }
-    sim[, , j, 1] <- M1 %*% sim[, , j, 1]
-    sim[, , j, 2] <- M2 %*% sim[, , j, 2]
-    sim[, , j, 3] <- M3 %*% sim[, , j, 3]
   }
-  # Return simulated reconciled sample paths
-  return(sim)
+  # Save results to file and then return them
+  write_rds(sim, filename)
+  return(invisible(sim))
+}
+
+# reconcile_sample_paths = Reconcile simulated future sample paths
+
+# object = gts object
+# model_function = function used to model each time series. e.g., ets or auto.arima or tscount
+# method = method of reconciliation
+
+reconcile_sample_paths <- function(object, model_function = "ets", methods = c("bu", "wls", "mint")) {
+  ntime <- NROW(object$bts)
+  
+  # Has this already been run?
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, 
+                                  "_sim_",methods[1],".rds"))
+  # Check if this has already been run
+  if (fs::file_exists(filename)) {
+    return(read_rds(filename))
+  }
+
+  # Otherwise reconcile for all the methods, and return the first one
+  sim <- future_sample_paths(object, model_function)
+  M <- make_mapping_matrices(object, model_function)
+
+  nsim <- dim(sim)[3]
+  filestem <- here::here(paste0("rscript/hts/", model_function, "_", ntime, "_sim_"))
+  for(k in rev(seq_along(methods))) {
+    newsim <- sim
+    for(j in seq(nsim)) {
+      newsim[,,j] <- M[[k]] %*% sim[,,j]
+    }
+    write_rds(newsim, paste0(filestem,methods[k],".rds"))
+  }
+  return(invisible(newsim))
 }
 
 # Compute RMSSE given forecast mean and training/test gts objects
-rmsse <- function(sim, test_gts, train_gts) {
-  fmean <- apply(sim, c(1, 2, 4), mean)
+
+# train_gts = gts object training data
+# test_gts = gts object test data
+# model_function = function used to model each time series. e.g., ets or auto.arima or tscount
+# method = method of reconciliation
+
+rmsse <- function(train_gts, test_gts, model_function, method) {
+  # Check if this has already been run
+  filename <- here::here(paste0("rscript/hts/", model_function, "_", ntime, 
+                                "_rmsse_",method,".rds"))
+  if (fs::file_exists(filename)) {
+    return(read_rds(filename))
+  }
+  
+  # Find the simulation files that exist
+  sim <- reconcile_sample_paths(train_gts, model_function, methods = method)
+
+  # Set up rmsse object
+  fmean <- apply(sim, c(2, 1), mean)
   alltest <- aggts(test_gts)
   alltrain <- aggts(train_gts)
   scale_factor <- colMeans(diff(alltrain, 7)^2)
-  rmsse <- fmean[, 1, ]
   for (i in NCOL(rmsse)) {
     rmsse[, i] <- colMeans(sweep((t(fmean[, , i]) - alltest)^2, 2L, scale_factor, "/"))
   }
