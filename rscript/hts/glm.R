97d4e21a15f3ca56f3921a64078a692b28737c21
Rob Hyndman <robjhyndman@gmail.com>
2022-10-28 11:24:14 +1100

    Stylistic changes only

--- rscript/hts/glm.R
+++ rscript/hts/glm.R
@@ -1,39 +1,44 @@
 # Specific tsglm model used for this data
 iglm <- function(y) {
   n <- length(y)
-  fourier_year <- forecast::fourier(ts(y, frequency=365.25), K=3)
+  fourier_year <- forecast::fourier(ts(y, frequency = 365.25), K = 3)
   season_week <- forecast::seasonaldummy(y)
-  trend <- splines::ns(seq(n), df=round(n/100))
-  X <- cbind(trend, season_week, fourier_year, holidays[seq_along(y),])
+  trend <- splines::ns(seq(n), df = round(n / 100))
+  X <- cbind(trend, season_week, fourier_year, holidays[seq_along(y), ])
   # Remove constant covariates
   constant <- apply(X, 2, forecast:::is.constant)
   X <- X[, !constant]
-  object <- suppressWarnings(try(glm(y ~ X, family=poisson()), silent=TRUE))
-  if(inherits(object, "try-error"))
+  object <- suppressWarnings(try(glm(y ~ X, family = poisson()), silent = TRUE))
+  if (inherits(object, "try-error")) {
     object <- glm(y ~ 1, family = poisson())
+  }
   # Slim down return object and add ts attributes
-  object <- list(coefficients=object$coefficients, 
-                 residuals=residuals(object, type="response"),
-                 y=y)
-  return(structure(object, class='iglm'))
+  object <- list(
+    coefficients = object$coefficients,
+    residuals = residuals(object, type = "response"),
+    y = y
+  )
+  return(structure(object, class = "iglm"))
 }
 
-simulate.iglm <- function(object, innov,...) {
+simulate.iglm <- function(object, innov, ...) {
   n <- length(object$y)
   h <- NROW(innov)
-  fourier_year <- forecast::fourier(ts(seq(n), frequency=365.25), K=3, h=h)
-  season_week <- forecast::seasonaldummy(ts(seq(n), frequency=7), h=h)
+  fourier_year <- forecast::fourier(ts(seq(n), frequency = 365.25), K = 3, h = h)
+  season_week <- forecast::seasonaldummy(ts(seq(n), frequency = 7), h = h)
   # Use last value of trend for future
-  lasttrend <- tail(splines::ns(seq(n), df=round(n/100)),1)
-  trend <- matrix(rep(lasttrend,h), nrow=h, byrow=TRUE)
+  lasttrend <- tail(splines::ns(seq(n), df = round(n / 100)), 1)
+  trend <- matrix(rep(lasttrend, h), nrow = h, byrow = TRUE)
   colnames(trend) <- colnames(lasttrend)
-  X <- cbind(rep(1, h), trend, season_week, fourier_year, holidays[n+seq(h),])
-  colnames(X) <- c("(Intercept)", paste0("X",colnames(X[,-1])))
+  X <- cbind(rep(1, h), trend, season_week, fourier_year, holidays[n + seq(h), ])
+  colnames(X) <- c("(Intercept)", paste0("X", colnames(X[, -1])))
   # Remove missing columns
-  X <- X[, colnames(X) %in% names(coefficients(object)), drop=FALSE]
+  X <- X[, colnames(X) %in% names(coefficients(object)), drop = FALSE]
   # Find mean of future periods
-  mean_future <- c(exp(X %*% matrix(coefficients(object), ncol=1)))
+  mean_future <- c(exp(X %*% matrix(coefficients(object), ncol = 1)))
   # Return Poisson series with this mean
-  ts(rpois(h, lambda=mean_future), frequency = frequency(object$y),
-     start = tsp(object$y)[2] + 1/frequency(object$y))
+  ts(rpois(h, lambda = mean_future),
+    frequency = frequency(object$y),
+    start = tsp(object$y)[2] + 1 / frequency(object$y)
+  )
 }
--- rscript/hts/tscount.R
+++ rscript/hts/tscount.R
@@ -2,43 +2,45 @@ library(tscount)
 
 # Specific tsglm model used for this data
 tscount <- function(y) {
-  fourier_year <- forecast::fourier(ts(y, frequency=365.25), K=3)
+  fourier_year <- forecast::fourier(ts(y, frequency = 365.25), K = 3)
   season_week <- forecast::seasonaldummy(y)
-  trend <- splines::ns(seq(n), df=round(n/100))
-  X <- cbind(trend, season_week, fourier_year, holidays[seq_along(y),])
+  trend <- splines::ns(seq(n), df = round(n / 100))
+  X <- cbind(trend, season_week, fourier_year, holidays[seq_along(y), ])
   # Remove constant covariates
   constant <- apply(X, 2, forecast:::is.constant)
   X <- X[, !constant]
-  tsglm(y, model = list(past_obs = 1:3), xreg=X, link="log")
+  tsglm(y, model = list(past_obs = 1:3), xreg = X, link = "log")
 }
 
-simulate.tsglm <- function(object, innov,...) {
+simulate.tsglm <- function(object, innov, ...) {
   n <- length(object$response)
   h <- NROW(innov)
-  fourier_year <- forecast::fourier(ts(seq(n), frequency=365.25), K=3, h=h)
-  season_week <- forecast::seasonaldummy(ts(seq(n), frequency=7), h=h)
+  fourier_year <- forecast::fourier(ts(seq(n), frequency = 365.25), K = 3, h = h)
+  season_week <- forecast::seasonaldummy(ts(seq(n), frequency = 7), h = h)
   # Use last value of trend for future
-  lasttrend <- tail(splines::ns(seq(n), df=round(n/100)),1)
-  trend <- matrix(rep(lasttrend,h), nrow=h, byrow=TRUE)
+  lasttrend <- tail(splines::ns(seq(n), df = round(n / 100)), 1)
+  trend <- matrix(rep(lasttrend, h), nrow = h, byrow = TRUE)
   colnames(trend) <- colnames(lasttrend)
-  X <- cbind(trend, season_week, fourier_year, holidays[n+seq(h),])
+  X <- cbind(trend, season_week, fourier_year, holidays[n + seq(h), ])
   # Remove missing columns
   X <- X[, colnames(X) %in% names(coefficients(object))]
   output <- tsglm.sim(h,
-        param = list(
-          intercept = object$coefficient[1],
-          past_obs = object$coefficients[2:4],
-          past_mean = FALSE,
-          xreg = object$coefficients[-(1:4)]
-        ),
-        model = list(
-          past_obs = 1:3,
-          past_mean = FALSE,
-          external=FALSE
-        ),
-        xreg = X,
-        link = "log"
+    param = list(
+      intercept = object$coefficient[1],
+      past_obs = object$coefficients[2:4],
+      past_mean = FALSE,
+      xreg = object$coefficients[-(1:4)]
+    ),
+    model = list(
+      past_obs = 1:3,
+      past_mean = FALSE,
+      external = FALSE
+    ),
+    xreg = X,
+    link = "log"
   )$ts
-  ts(output, frequency = frequency(object$response),
-     start = tsp(object$response)[2] + 1/frequency(object$response))
+  ts(output,
+    frequency = frequency(object$response),
+    start = tsp(object$response)[2] + 1 / frequency(object$response)
+  )
 }
